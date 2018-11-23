472d3328f65f3e6d14b0b779d957a31277ec2149
Juuha <juha.ritakoski@hotmail.com>
2018-11-23 15:13:34 +0200

    tehtävä 3.12 tehty

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+const user = process.env.fsoweek3user || '<username>'
+const pass = process.env.fsoweek3pass || '<password>'
+
+const url = `mongodb://${user}:${pass}@ds139690.mlab.com:39690/puhelinnumerodb`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+switch (process.argv.length){
+  case 2:
+    console.log('puhelinluettelo:')
+    Person
+      .find({})
+      .then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      })
+    break
+  case 4:
+      const person = new Person({
+      name: process.argv[2],
+      number: process.argv[3]
+    })
+
+    person
+      .save()
+      .then(response => {
+        console.log(`lisätään henkilö ${person.name} numero ${person.number} luetteloon`)
+        mongoose.connection.close()
+      })
+    break
+  default:
+    console.log('Syötä 0 tai 2 parametriä.')
+    mongoose.connection.close()
+}
+
+
